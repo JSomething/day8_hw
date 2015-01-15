3fdf8a2d99db6e2e25d329307ba7284b96f7dac6
Jon <jondotcourtney@gmail.com>
2015-01-15 01:45:00 -0500

    ultimate AMC Pacer

--- server.rb
+++ server.rb
@@ -7,39 +7,18 @@ get '/' do
 	erb :index, layout: :format
 end
 
-@overlay_background = "./amc-pacer-6.jpg"
+get '/history' do
+	erb :history, layout: :format
+end
 
-# get '/' do
-# 	@suspects = [
-# 	{
-# 		name: Faker::Name.name,
-# 		address: Faker::Address.city
-# 		}
-# 	]
-# 	erb :index, layout: :format 	
-# end
+get '/dealers' do
+	erb :history, layout: :format
+end
 
-# get '/about' do
-# 	erb :about, layout: :format
-# end
+get '/history' do
+	erb :index, layout: :format
+end
 
-# get '/suspects' do
-# 	@suspects = [
-# 		{
-# 			name: Faker::Name.name,
-# 			address_street: Faker::Address.street_name, 
-# 			address_city: Faker::Address.city,
-# 			advice: Faker::Hacker.say_something_smart
-# 		},
-# 		{
-# 			name: Faker::Name.name,
-# 			address_street: Faker::Address.street_name, 
-# 			address_city: Faker::Address.city,
-# 			advice: Faker::Hacker.say_something_smart
-# 		}
-# 	]
-# 	erb :suspects, layout: :format
-# end
 
 # get '/form' do
 # 	erb :form, layout: :format
